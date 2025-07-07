bdb47f4fb4496eb508a818360b3215b02bb18842
itzkhalid04 <itzkhalid04@gmail.com>
2025-07-07 09:47:01 +0530

    add

--- app/src/main/java/com/myrat/app/data/Sms.kt
+++
@@ -1,9 +0,0 @@
-package com.myrat.app.data
-
-import com.google.gson.annotations.SerializedName
-
-data class Sms(
-    @SerializedName("sender") val sender: String,
-    @SerializedName("message") val message: String,
-    @SerializedName("timestamp") val timestamp: Long
-)
